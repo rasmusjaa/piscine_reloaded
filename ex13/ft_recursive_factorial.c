dfce09d110c555e785d7d5d2a3d5420d4546a317
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-14 17:53:35 +0300

    ex13 done

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 17:32:40 by rjaakonm          #+#    #+#             */
+/*   Updated: 2019/10/14 17:51:30 by rjaakonm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 1 || nb == 0)
+		return (1);
+	else if (nb > 1 && nb < 12)
+		return (nb * ft_recursive_factorial(nb - 1));
+	else
+		return (0);
+}
