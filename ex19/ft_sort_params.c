441de3a6aa5cdf891618ec09b2e9f5540e9999d7
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-14 19:33:49 +0300

    ex19 done

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 19:08:25 by rjaakonm          #+#    #+#             */
+/*   Updated: 2019/10/14 19:32:42 by rjaakonm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+
+	i = 1;
+	if (ac == 1)
+		return (0);
+	if (ac > 1)
+	{
+		while (i < (ac - 1))
+		{
+			if (ft_strcmp(av[i], av[i + 1]) == 1)
+			{
+				ft_swap(&av[i], &av[i + 1]);
+				i = 1;
+			}
+			i++;
+		}
+	}
+	i = 0;
+	while (i < ac)
+	{
+		ft_putstr(av[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
