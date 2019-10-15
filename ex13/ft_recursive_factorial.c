5d0242406234da572b83b4cd54943e3d3161b026
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-15 14:10:02 +0300

    ex13 biggest int from 12 to 13

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 17:32:40 by rjaakonm          #+#    #+#             */
-/*   Updated: 2019/10/14 17:51:30 by rjaakonm         ###   ########.fr       */
+/*   Updated: 2019/10/15 14:09:49 by rjaakonm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_recursive_factorial(int nb)
 {
 	if (nb == 1 || nb == 0)
 		return (1);
-	else if (nb > 1 && nb < 12)
+	else if (nb > 1 && nb < 13)
 		return (nb * ft_recursive_factorial(nb - 1));
 	else
 		return (0);
