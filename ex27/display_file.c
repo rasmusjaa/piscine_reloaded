1e8b0acc6a65921fff4f021fcf5fccf3af157bf3
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-15 15:43:34 +0300

    ex27 done

---
+++ ex27/display_file.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/15 14:26:40 by rjaakonm          #+#    #+#             */
+/*   Updated: 2019/10/15 15:37:55 by rjaakonm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int	main(int ac, char **av)
+{
+	char	buf[10];
+	int		fd;
+
+	if (ac == 1)
+	{
+		write(2, "File name missing.\n", 19);
+		return (1);
+	}
+	if (ac > 2)
+	{
+		write(2, "Too many arguments.\n", 20);
+		return (1);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+	while (read(fd, buf, 10) != 0)
+		write (1, buf, 10);
+	close (fd);
+	return (0);
+}
