1b90a2559890e84536fc736ec83c168eb79ba22b
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-15 11:38:57 +0300

    ex20 done

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/15 10:51:07 by rjaakonm          #+#    #+#             */
+/*   Updated: 2019/10/15 11:06:17 by rjaakonm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		len;
+
+	len = 0;
+	while (src[len])
+		len++;
+	dest = (char*)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	len = 0;
+	while (src[len])
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
