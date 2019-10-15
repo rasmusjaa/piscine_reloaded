27127a56bb3a7c8718776851408b74b1ab523603
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2019-10-15 14:09:38 +0300

    ex12 biggest int from 12 to 13

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 17:21:26 by rjaakonm          #+#    #+#             */
-/*   Updated: 2019/10/14 17:55:44 by rjaakonm         ###   ########.fr       */
+/*   Updated: 2019/10/15 14:09:04 by rjaakonm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_iterative_factorial(int nb)
 	int i;
 
 	i = 0;
-	if (nb >= 0 && nb < 12)
+	if (nb >= 0 && nb < 13)
 	{
 		i = 1;
 		while (nb > 0)
